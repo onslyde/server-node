4dac5445ff7b471ed9a646eb9e20452155dba29d
Fred Stakem <fstakem@gmail.com>
2012-05-06 14:57:48 -0400

    Finished a quarter of the the state machine using a tree data structure.

--- src/presentationStateMachine.js
+++ src/presentationStateMachine.js
@@ -8,38 +8,118 @@ var Slide = require("./slide");
 function PresentationStateMachine(name)
 {
 	this.name = name;
-	this.current_node = null;
+	this.current_slide_node = null;
 	this.slide_tree = null;
 };
 
 PresentationStateMachine.prototype =
 {
+	setSlideTree:			function(slide_tree)
+	{
+		if(slide_tree != null)
+		{
+			this.slide_tree = slide_tree;
+			this.current_slide_node = this.slide_tree; 
+		}
+	},
+		
 	start:					function()
 	{
-		// TODO
+		console.log("Starting the presentation.");
+		
+		if(this.slide_tree != null)
+		{
+			this.current_slide_node = this.slide_tree;
+			slide = this.current_slide_node.data;
+			slide.enterSlide();
+			console.log(this.toString());
+			return slide;
+		}
+		
+		console.log("Error starting the presentation because no slide tree present.");
+		console.log(this.toString());
+		
+		return null;
 	},
 	
 	restart:				function()
 	{
-		// TODO
+		this.start();
 	},
 	
 	getFirstSlide:			function()
 	{
-		// TODO
+		console.log("Getting the first slide.");
+		
+		if(this.slide_tree != null)
+		{
+			this.current_slide_node = this.slide_tree;
+			slide = this.current_slide_node.data;
+			slide.enterSlide();
+			console.log(this.toString());
+			return slide;
+		}
+		
+		console.log("Error getting the first slide because no slide tree present.");
+		console.log(this.toString());
+		
+		return null;
 	},
 	
 	getLastSlide:			function()
 	{
-		// TODO
+		console.log("Getting the last slide.");
+		
+		// Default last slide is the slide from the lowest branches
+		if(this.slide_tree != null)
+		{
+			this.current_slide_node = this.slide_tree;
+			this.current_slide_node = this.current_slide_node.children[0];
+			while(this.current_slide_node != undefined)
+			{
+				this.current_slide_node = this.current_slide_node.children[0];
+			}
+			
+			this.current_slide_node = this.current_slide_node.parent;
+			if(this.current_slide_node == null)
+			{
+				this.current_slide_node = this.slide_tree;
+			}
+			
+			slide = this.current_slide_node.data;
+			slide.enterSlide();
+			console.log(this.toString());
+			return slide;
+		}
+		
+		console.log("Error getting the last slide because no slide tree present.");
+		console.log(this.toString());
+		
+		return null;
 	},
 	
 	getNextSlide:			function()
 	{
-		// TODO
+		// TODO -> Finish this
+		console.log("Getting the next slide.");
+		
+		if(this.slide_tree != null)
+		{
+			
+		}
+		
+		console.log("Error getting the next slide because no slide tree present.");
+		console.log(this.toString());
+		
+		return null;
 	},
 	
 	getPreviousSlide:		function()
+	{
+		// TODO
+	},
+	
+	getCurrentSlide:		function()
 	{
 		// TODO
 	},
@@ -57,6 +137,7 @@ PresentationStateMachine.prototype =
 	toString:				function()
 	{
 		// TODO
+		return "---Presentation State---";
 	}
 };
 
