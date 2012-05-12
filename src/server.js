9420c7cd652556ae1674c8ef73373312c42ec1f3
Fred Stakem <fstakem@gmail.com>
2012-05-12 11:29:28 -0400

    Finished the first draft of the server with the new state machine. Need to clean up the presentation class and make it hold more info.

--- src/server.js
+++ src/server.js
@@ -11,14 +11,14 @@ function start(route, handle)
 	// Slide deck variables
 	var presentation = createPresentation();
 	var presentation_state_machine = presentation.state_machine;
-	//slide_deck.start();
+	presentation_state_machine.start();
 	
 	// Server variables
 	var port = 8080;
 	
 	function onRequest(request, response) 
 	{
-		route(slide_deck, handle, request, response);
+		route(presentation_state_machine, handle, request, response);
 	}
 
 	http.createServer(onRequest).listen(port);
@@ -29,25 +29,84 @@ function createPresentation()
 {
 	var presentation = new Presentation("Test Presentation");
 	var presentation_state_machine = new PresentationStateMachine("Test State Machine");
-	
-	// Create slide tree 
+	 
 	// TODO make this a JSON object that is parsed
-	var state_tree = 
+	// A -> B -> C
+	//           |-> D -> E -> F
+	//           |-> G -> H
+	//           |-> I -> J -> K
+	//                         |-> L -> M
+	//                         |-> N -> O
 	
+	// First leg
+	var a = Arboreal();
+	a.data = new Slide("A");
 	
+	a.appendChild();
+	b = a.children[0];
+	b.data = new Slide("B");
 	
-	var slide_deck = new SlideDeck("Test Slide Deck");
-	var slides = new Array();
+	b.appendChild();
+	c = b.children[0];
+	c.data = new Slide("C");
 	
-	for(i = 0; i < 4; i++)
-	{
-		slide = new Slide("Slide " + (i));
-		slides.push(slide);
-	}
+	// Second leg
+	c.appendChild();
+	d = c.children[0];
+	d.data = new Slide("D");
 	
-	slide_deck.setSlides(slides);
+	d.appendChild();
+	e = d.children[0];
+	e.data = new Slide("E");
 	
-	return slide_deck;
+	e.appendChild();
+	f = e.children[0];
+	f.data = new Slide("F");
+	
+	// Third leg
+	c.appendChild();
+	g = c.children[0];
+	g.data = new Slide("G");
+	
+	g.appendChild();
+	h = g.children[0];
+	h.data = new Slide("H");
+	
+	// Fourth leg
+	c.appendChild();
+	i = c.children[0];
+	i.data = new Slide("I");
+	
+	i.appendChild();
+	j = i.children[0];
+	j.data = new Slide("J");
+	
+	j.appendChild();
+	k = j.children[0];
+	k.data = new Slide("K");
+	
+	// Fifth leg
+	k.appendChild();
+	l = k.children[0];
+	l.data = new Slide("L");
+	
+	l.appendChild();
+	m = l.children[0];
+	m.data = new Slide("M");
+	
+	// Sixth leg
+	k.appendChild();
+	n = k.children[0];
+	n.data = new Slide("N");
+	
+	n.appendChild();
+	o = n.children[0];
+	o.data = new Slide("O");
+	
+	presentation_state_machine.slide_tree(state_tree_root);
+	presentation.state_machine = presentation_state_machine;
+	
+	return presentation;
 }
 
 exports.start = start;
